34a5de030e038844ad820f87131f126910182ab6
Evan Heidtmann <evan.heidtmann@gmail.com>
2016-04-07 11:38:59 -0700

    Add arraylist example code

--- MyDemo.java
+++ MyDemo.java
@@ -2,11 +2,13 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.PointerType;
 
+import java.util.ArrayList;
+
 public class MyDemo {
   public static class StuffPointer extends PointerType {}
 
-  public static native StuffPointer createThing();
-  public static native void updateThing(StuffPointer s, int N, float F);
+  public static native StuffPointer createThing(int size);
+  public static native void updateThing(StuffPointer s, int N, float F, float[] values);
   public static native void printThing(StuffPointer s);
 
   static {
@@ -14,9 +16,22 @@ public class MyDemo {
   }
 
   public static void main(String[] args) {
-    StuffPointer s = createThing();
+    ArrayList<Float> a = new ArrayList<Float>();
+    a.add(1.f);
+    a.add(2.f);
+    a.add(4.f);
+
+    StuffPointer s = createThing(a.size());
     printThing(s);
-    updateThing(s, 10, 0.4f);
+
+    // We have to manually convert to a primitive array, thanks Java.
+    float[] fa = new float[a.size()];
+    int i = 0;
+    for (Float f : a) {
+      fa[i++] = (f == null ? Float.NaN : f);
+    }
+
+    updateThing(s, 10, 0.4f, fa);
     printThing(s);
   }
 }
--- mystuff/mystuff.c
+++ mystuff/mystuff.c
@@ -6,22 +6,40 @@ typedef struct MyStruct MyStruct;
 struct MyStruct {
   int N;
   float F;
+  int size;
+  float *A;
 };
 
-MyStruct* createThing() {
+MyStruct* createThing(int size) {
   MyStruct* s = (MyStruct*) malloc(sizeof(struct MyStruct));
   s->N = 0;
   s->F = 0.f;
+  s->size = size;
+
+  s->A = (float*) malloc(sizeof(int) * size);
+
+  for (int i = 0; i < size; ++i) {
+    s->A[i] = 0.f;
+  }
+
   return s;
 }
 
-void updateThing(MyStruct* s, int newN, float newFloat)
+void updateThing(MyStruct* s, int newN, float newFloat, float* values)
 {
   s->N = newN;
   s->F = newFloat;
+
+  for (int i = 0; i < s->size; ++i) {
+    s->A[i] = values[i];
+  }
 }
 
 void printThing(MyStruct* s) {
   printf("s->N: %d\n", s->N);
   printf("s->F: %f\n", (double) s->F);
+
+  for (int i = 0; i < s->size; ++i) {
+    printf("s->A[%d]: %f\n", i, (double) s->A[i]);
+  }
 }
